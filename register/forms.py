0ed4a2a0a2c0acb34740c8e1a2878666cac8231f
Your Name <you@example.com>
2024-06-24 16:58:14 +0700

    initial commit

---
+++ register/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    birth_date = forms.DateField(required=True, widget=forms.SelectDateWidget(years=range(1900, 2024)))
+    height = forms.FloatField(required=True)
+    weight = forms.FloatField(required=True)
+    dropdown_field = forms.ChoiceField(choices=[
+        ('gluten-free', 'Gluten-Free'),
+        ('diabetes-friendly', 'Diabetes'),
+        ('low-sodium', 'Low-Sodium'),
+        ('autism-friendly', 'Autism')
+        ])
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'birth_date', 'height', 'weight', 'dropdown_field']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email is already in use.")
+        return email
---
+++ register/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CreateUserForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # Load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.profile.height = form.cleaned_data.get('height')
+            user.profile.weight = form.cleaned_data.get('weight')
+            user.profile.dropdown_field = form.cleaned_data.get('dropdown_field')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            
+            # Authenticate the user after registration
+            user = authenticate(username=user.username, password=raw_password)
+            if user is not None:
+                login(request, user)  # Log in the user
+                return redirect('login')  # Redirect to the success page after login
+    else:
+        form = CreateUserForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)  # Correct usage of login function
+                return redirect('home')  # Redirect to the success page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
